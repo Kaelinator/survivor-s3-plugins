c7c86acddf0bfd869fa7b231f03820e37ef5bb63
BuildTools <kirkkael@gmail.com>
2020-06-15 04:59:32 -0500

    added tab completion

--- src/main/java/com/kaelkirk/commands/WorldTeleportCommand.java
+++ src/main/java/com/kaelkirk/commands/WorldTeleportCommand.java
@@ -1,12 +1,18 @@
 package com.kaelkirk.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.kaelkirk.Plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class WorldTeleportCommand implements CommandExecutor {
+public class WorldTeleportCommand implements CommandExecutor, TabCompleter {
   Plugin plugin;
 
   public WorldTeleportCommand(Plugin plugin) {
@@ -15,8 +21,19 @@ public class WorldTeleportCommand implements CommandExecutor {
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    
-    sender.sendMessage("Okay then!");
+
+    sender.sendMessage("Teleporting to " + args[0] + ".");
     return false;
+  }
+
+  @Override
+  public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    List<String> suggestions = new ArrayList<String>();
+
+    for (World world : Bukkit.getServer().getWorlds()) {
+      suggestions.add(world.getName());
+    }
+
+    return suggestions;
   }
 }
