9a6b30c1e98b984a93d3c9149f82c93f6df075af
BuildTools <kirkkael@gmail.com>
2020-07-01 23:59:32 -0500

    added player honor scoreboard

--- src/main/java/com/kaelkirk/machines/duels/DuelScoreboard.java
+++ src/main/java/com/kaelkirk/machines/duels/DuelScoreboard.java
@@ -1,10 +1,16 @@
 package com.kaelkirk.machines.duels;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class DuelScoreboard implements Listener {
   
@@ -23,5 +29,21 @@ public class DuelScoreboard implements Listener {
     if (honor == null) {
       duelMachine.setPlayerHonor(player, plugin.getConfig().getInt("initialHonor"));
     }
+    displayHonorScoreboard(player);
+  }
+
+  /**
+   * Sets the honor's display to the sidebar for player p.
+   * 
+   * @param p the player to set the honor display for
+   */
+  private void displayHonorScoreboard(Player p) {
+    ScoreboardManager manager = Bukkit.getScoreboardManager();
+    Scoreboard board = manager.getNewScoreboard();
+    Objective displayObj = board.registerNewObjective("display", "dummy", " ");
+    displayObj.setDisplaySlot(DisplaySlot.SIDEBAR);
+    Score score = displayObj.getScore("Honor:");
+    score.setScore(duelMachine.getPlayerHonor(p));
+    p.setScoreboard(board);
   }
 }
